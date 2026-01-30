dab3215fabcf84c7814a7a19a915ea18199927bd
QUADEKDEV <quadek50@gmail.com>
2026-01-30 10:26:20 +0100

    update

--- proxy.ts
+++ proxy.ts
@@ -1,21 +1,16 @@
-import { NextRequest, ProxyConfig, NextResponse } from "next/server";
+import { NextRequest,ProxyConfig,NextResponse } from "next/server";
 import { auth } from "./app/utils/session";
 
-// const publicRoutes=["/","/signin","/signup","/about","/ts","/users"]
 export default async function proxy(req: NextRequest) {
-  // const path =req.nextUrl.pathname
-  // const isPublicRoute=publicRoutes.includes(path);
-  // if(isPublicRoute){
-  //   return NextResponse.next();
-  // }
+
   const { success } = await auth();
   if (!success) {
-    return NextResponse.redirect(new URL("/signin", req.nextUrl));
+    return NextResponse.redirect(new URL("/login", req.nextUrl));
   }
   return NextResponse.next();
 }
 export const config: ProxyConfig = {
   matcher: ["/admindashboard","/userdashboard"],
-  // matcher: ["/((?!api|_next/static|_next/image|.*\\.png$).*)"]
+
 };
 
