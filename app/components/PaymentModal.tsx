2d22d00c2d1920f591600e018cb57f23579577cf
QUADEKDEV <quadek50@gmail.com>
2026-01-31 21:28:25 +0100

    update

--- app/components/PaymentModal.tsx
+++ app/components/PaymentModal.tsx
@@ -1,3 +1,4 @@
+"use client"
 import React, { useState,useEffect} from "react";
 import {Calendar,Check,X,CreditCard,Loader2,} from "lucide-react";
 import { motion} from "framer-motion";
--- app/components/RoomCard.tsx
+++ app/components/RoomCard.tsx
@@ -1,3 +1,4 @@
+"use client"
 import {MapPin,Users,}from "lucide-react";
 import { motion, AnimatePresence } from "framer-motion";
 
